8ecbf89c736ec264e46ce853aebf8ef61d963389
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-12 04:31:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -73,8 +73,8 @@ def get_average_age_for_season(data, season)
       contestants.each do |contestant|
         binding.pry
         contestant.each do |attribute, value|
-          binding.pry
           if attribute == "age"
+            binding.pry
             contestant_age = value.to_i
             ages_sum += contestant_age
             num_of_contestants += 1
